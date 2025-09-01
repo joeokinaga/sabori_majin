66be0b3ae552f4c88fd197ba5fb4feeea75f733c
okinagajoe <joeokinaga0921@gmail.com>
2025-09-01 16:16:10 +0900

    wip

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,25 @@
 class TasksController < ApplicationController
+    before_action :authenticate_user!
+
     def index 
+      @tasks = current_user.tasks.where(planned_start_at: Date.current.all_day).order(planned_start_at: :asc)
     end
 
     def show
+      @task = current_user.tasks.find(params[:id])
     end
 
     def new
-        @task = Task.new
+      @task = current_user.tasks.build
     end
 
     def create
+      @task = current_user.tasks.build(task_params)
+      if @task.save
+        redirect_to tasks_path, notice: "タスクを作成しました"
+      else
+        render :new
+      end
     end
 
     def edit
@@ -20,4 +30,9 @@ class TasksController < ApplicationController
 
     def destroy
     end
+
+    private
+        def task_params
+        params.require(:task).permit(:title, :content, :planned_start_at, :planned_finish_at)
+        end
 end
