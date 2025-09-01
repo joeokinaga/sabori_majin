66f61c02dd1952dfe073f676c4363a0073997b19
okinagajoe <joeokinaga0921@gmail.com>
2025-09-01 17:07:22 +0900

    タスク新規作成フォーム(バリデーションなし)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,7 +14,19 @@ class TasksController < ApplicationController
     end
 
     def create
+      date = params[:task][:planned_date]
+      start_hour = params[:task][:start_hour]
+      start_minute = params[:task][:start_minute]
+      finish_hour = params[:task][:finish_hour]
+      finish_minute = params[:task][:finish_minute]
+      
+      planned_start_at = Time.zone.parse("#{date} #{start_hour}:#{start_minute}")
+      planned_finish_at = Time.zone.parse("#{date} #{finish_hour}:#{finish_minute}")
+      
       @task = current_user.tasks.build(task_params)
+      @task.planned_start_at = planned_start_at
+      @task.planned_finish_at = planned_finish_at
+      
       if @task.save
         redirect_to tasks_path, notice: "タスクを作成しました"
       else
