3f5feddb9aa10901251aebf3db8ae4f6e294f518
松野康世 <matsunokousei@matsunoyasuyonoMacBook-Pro.local>
2025-09-01 17:14:19 +0900

    userモデルにvalidation追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_01_045733) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_01_080804) do
   create_table "tasks", force: :cascade do |t|
     t.text "content"
     t.integer "status", default: 0, null: false
@@ -33,7 +33,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_01_045733) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
