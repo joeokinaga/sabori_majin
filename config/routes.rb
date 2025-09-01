254d14689a3305a81ba24216fd7d6a2044267620
okinagajoe <joeokinaga0921@gmail.com>
2025-09-01 13:28:49 +0900

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts, only: [:index]
+  resources :tasks
   root 'tasks#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
