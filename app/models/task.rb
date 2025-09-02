94773339bcb5c1911ece8b7a7052282c3b048f62
okinagajoe <joeokinaga0921@gmail.com>
2025-09-02 11:01:58 +0900

    add some validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,28 @@ class Task < ApplicationRecord
    validates :title, presence: true
    validates :planned_start_at, presence: true
    validates :planned_finish_at, presence: true
+   validate :finish_after_start
+   validate :no_overlap, if: -> { planned_start_at.present? && planned_finish_at.present? }
+
+   private
+
+    def finish_after_start
+        return if planned_start_at.blank? || planned_finish_at.blank?
+        if planned_finish_at <= planned_start_at
+            errors.add(:planned_finish_at, "Finishing time must be after starting time")
+        end
+    end
+
+    def no_overlap
+        overlap = Task
+                    .where(user_id: user_id)               # 同じユーザー
+                    .where.not(id: id)                     # 自分自身は除外（更新時用）
+                    .where(
+                        "(planned_start_at < ? AND planned_finish_at > ?)",
+                        planned_finish_at, planned_start_at
+                    )
+        if overlap.exists?
+            errors.add(:base, "It overlaps with another task's time")
+        end
+    end
 end
