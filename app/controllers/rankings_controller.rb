e5a7d38244bae555603af39142ebe5a5e8b29bd5
松野康世 <matsunokousei@matsunoyasuyonoMacBook-Pro.local>
2025-09-04 12:07:55 +0900

    lazyスコアを算出して並び替えて表示

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,4 +1,12 @@
 class RankingsController < ApplicationController
+  include LazyScoreCalculator
+  before_action :authenticate_user!, only: [:show]
   def show
+    @all_users = User.all
+    @all_users.each do |user|
+      tasks = user.tasks.where("planned_start_at <= ?", Time.current)
+      user.update(lazy_score: sum_lazy_score(tasks))
+    end
+    @all_users = @all_users.order(lazy_score: :desc)
   end
 end
