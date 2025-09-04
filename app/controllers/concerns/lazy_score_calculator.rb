f77de54a5caa4c3d1964b9ed2e7da57889f57af2
松野康世 <matsunokousei@matsunoyasuyonoMacBook-Pro.local>
2025-09-05 00:47:11 +0900

    サボりスコア算出修正

--- app/controllers/concerns/lazy_score_calculator.rb
+++ app/controllers/concerns/lazy_score_calculator.rb
@@ -8,25 +8,27 @@ module LazyScoreCalculator
     def calc_lazy_score(task)
         if task.status != "unstarted" || Time.current > task.planned_start_at + 3.minutes
             if task.status == "unstarted"
-            return 5
+                return 5
+            elsif task.status == "working"
+                return 0
             else
-            lazy_pt = 0
+                lazy_pt = 0
 
-            if task.status == "stopped"
-                lazy_pt += 2
-            end
+                if task.status == "stopped"
+                    lazy_pt += 2
+                end
 
-            if task.started_at > task.planned_start_at + 3.minutes
-                lazy_pt += 2
-            elsif task.started_at < task.planned_start_at - 3.minutes
-                lazy_pt += 1
-            end
+                if task.started_at > task.planned_start_at + 3.minutes
+                    lazy_pt += 2
+                elsif task.started_at < task.planned_start_at - 3.minutes
+                    lazy_pt += 1
+                end
 
-            if (task.planned_finish_at - task.planned_start_at) * 0.8 > (task.finished_at - task.started_at)
-                lazy_pt += 1
-            end
+                if (task.planned_finish_at - task.planned_start_at) * 0.8 > (task.finished_at - task.started_at)
+                    lazy_pt += 1
+                end
 
-            return lazy_pt
+                return lazy_pt
 
             end
         else
