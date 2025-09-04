e49600b4f954050cd8e5cc1bf945e6c2fcef2f81
松野康世 <matsunokousei@matsunoyasuyonoMacBook-Pro.local>
2025-09-05 00:38:59 +0900

    煽り追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_04_012843) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_04_133053) do
+  create_table "messages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.text "content"
     t.integer "status", default: 0, null: false
